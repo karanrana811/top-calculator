81741e09250fca9263396fe0589dc7341ef034d2
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-29 22:26:58 +0530

    add number buttons and sync their clicks with display div

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+const zero = document.querySelector('#zero')
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const numList = document.querySelectorAll('button')
+const display = document.querySelector('.display');
+
+const fillDisp = () => {
+    numList.forEach(num => {
+        num.addEventListener('click', (event) => {
+            let id = event.target.id
+            if (id === 'zero'){
+                if(display.textContent !== '0') display.textContent += '0'
+            } if (id === 'one'){
+                display.textContent === '0' ? display.textContent = '1' : display.textContent += '1'
+            }
+            if (id === 'two'){
+                display.textContent === '0' ? display.textContent = '2' : display.textContent += '2'
+            }
+            if (id === 'three'){
+                display.textContent === '0' ? display.textContent = '3' : display.textContent += '3'
+            }
+            if (id === 'four'){
+                display.textContent === '0' ? display.textContent = '4' : display.textContent += '4'
+            }
+            if (id === 'five'){
+                display.textContent === '0' ? display.textContent = '5' : display.textContent += '5'
+            }
+            if (id === 'six'){
+                display.textContent === '0' ? display.textContent = '6' : display.textContent += '6'
+            }
+            if (id === 'seven'){
+                display.textContent === '0' ? display.textContent = '7' : display.textContent += '7'
+            }
+            if (id === 'eight'){
+                display.textContent === '0' ? display.textContent = '8' : display.textContent += '8'
+            }
+            if (id === 'nine'){
+                display.textContent === '0' ? display.textContent = '9' : display.textContent += '9'
+            } 
+        })
+    })
+}
+
 let firstNum;
 let operator;
 let secondNum;
@@ -24,3 +73,4 @@ const operate = (op, numOne, numTwo) => {
     return returnVal;
 }
 
+fillDisp();
