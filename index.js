bf835a36625eeca673644dbbcbdab2abd03ff668
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-29 20:48:56 +0530

    connect all four math operators to a single function - operate

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const add = (...args) => {
-    let sum = 0;
-    for (let arg of args) sum += arg
-    return sum;
+let firstNum;
+let operator;
+let secondNum;
+
+const add = (a,b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const multiply = (a,b) => a*b;
+
+const divide = (a,b) => a/b;
+
+const operate = (op, numOne, numTwo) => {
+    let returnVal;
+    if(op === '+'){
+        returnVal = add(numOne, numTwo)
+    } else if(op === '-') {
+        returnVal = subtract(numOne, numTwo)
+    } else if(op === '*') {
+        returnVal = multiply(numOne, numTwo)
+    } else if(op === '/') {
+        returnVal = divide(numOne, numTwo)
+    }
+    return returnVal;
 }
+
