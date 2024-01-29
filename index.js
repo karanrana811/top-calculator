299c9c90f836256dc07841ac21d19438afaea9f7
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-29 22:27:50 +0530

    delete unused code

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const zero = document.querySelector('#zero')
-const one = document.querySelector('#one')
-const two = document.querySelector('#two')
-const three = document.querySelector('#three')
-const four = document.querySelector('#four')
-const five = document.querySelector('#five')
-const six = document.querySelector('#six')
-const seven = document.querySelector('#seven')
-const eight = document.querySelector('#eight')
 const numList = document.querySelectorAll('button')
 const display = document.querySelector('.display');
 
