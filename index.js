9901af074e16b9cff8084591566590f984e9bbfc
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-31 20:56:00 +0530

    wire addition function to number buttons

--- index.js
+++ index.js
@@ -1,64 +1,139 @@
 const numList = document.querySelectorAll('button')
 const display = document.querySelector('.display');
+let displayContent = 0;
+let firstNum;
+let operator;
+let secondNum;
+let prevClick;
+
 
 const fillDisp = () => {
     numList.forEach(num => {
         num.addEventListener('click', (event) => {
             let id = event.target.id
-            if (id === 'zero'){
-                if(display.textContent !== '0') display.textContent += '0'
-            } if (id === 'one'){
-                display.textContent === '0' ? display.textContent = '1' : display.textContent += '1'
+            if (id === 'zero') {
+                prevClick === 'add' ? display.textContent = 0 : display.textContent += '0';
+                prevClick = 'zero';
             }
-            if (id === 'two'){
-                display.textContent === '0' ? display.textContent = '2' : display.textContent += '2'
+            if (id === 'one') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 1;
+                } else {
+                    display.textContent += '1'
+                }
+                prevClick = 'one'
             }
-            if (id === 'three'){
-                display.textContent === '0' ? display.textContent = '3' : display.textContent += '3'
+            if (id === 'two') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 2;
+                } else {
+                    display.textContent += '2'
+                }
+                prevClick = 'two'
             }
-            if (id === 'four'){
-                display.textContent === '0' ? display.textContent = '4' : display.textContent += '4'
+            if (id === 'three') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 3;
+                } else {
+                    display.textContent += '3'
+                }
+                prevClick = 'three'
             }
-            if (id === 'five'){
-                display.textContent === '0' ? display.textContent = '5' : display.textContent += '5'
+            if (id === 'four') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 4;
+                } else {
+                    display.textContent += '4'
+                }
+                prevClick = 'four'
             }
-            if (id === 'six'){
-                display.textContent === '0' ? display.textContent = '6' : display.textContent += '6'
+            if (id === 'five') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 5;
+                } else {
+                    display.textContent += '5'
+                }
+                prevClick = 'five'
             }
-            if (id === 'seven'){
-                display.textContent === '0' ? display.textContent = '7' : display.textContent += '7'
+            if (id === 'six') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 6;
+                } else {
+                    display.textContent += '6'
+                }
+                prevClick = 'six'
             }
-            if (id === 'eight'){
-                display.textContent === '0' ? display.textContent = '8' : display.textContent += '8'
+            if (id === 'seven') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 7;
+                } else {
+                    display.textContent += '7'
+                }
+                prevClick = 'seven'
             }
-            if (id === 'nine'){
-                display.textContent === '0' ? display.textContent = '9' : display.textContent += '9'
-            } 
+            if (id === 'eight') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 8;
+                } else {
+                    display.textContent += '8'
+                }
+                prevClick = 'eight'
+            }
+            if (id === 'nine') {
+                if (prevClick === 'add' || display.textContent === '0') {
+                    display.textContent = 9;
+                } else {
+                    display.textContent += '9'
+                }
+                prevClick = 'nine'
+            }
+            if (id === 'add') {
+                firstNum = display.textContent;
+                operator = '+';
+                prevClick = 'add';
+            }
+            if (id === 'equal') {
+                if (prevClick != 'equal') {
+                    secondNum = display.textContent;
+                    display.textContent = (firstNum && operator) ? operate(operator, firstNum, secondNum) : 0;
+                }
+                prevClick = 'equal'
+            }
+            if (id === 'clear') {
+                display.textContent = 0;
+                firstNum = '';
+                secondNum = '';
+                operator = '';
+                prevClick = 'clear'
+            }
+            if (prevClick === 'equal') {
+                firstNum = '';
+                secondNum = '';
+                operator = '';
+            }
+
+            displayContent = display.textContent;
         })
     })
 }
 
-let firstNum;
-let operator;
-let secondNum;
+const add = (a, b) => +a + +b;
 
-const add = (a,b) => a + b;
+const subtract = (a, b) => a - b;
 
-const subtract = (a,b) => a - b;
+const multiply = (a, b) => a * b;
 
-const multiply = (a,b) => a*b;
-
-const divide = (a,b) => a/b;
+const divide = (a, b) => a / b;
 
 const operate = (op, numOne, numTwo) => {
     let returnVal;
-    if(op === '+'){
+    if (op === '+') {
         returnVal = add(numOne, numTwo)
-    } else if(op === '-') {
+    } else if (op === '-') {
         returnVal = subtract(numOne, numTwo)
-    } else if(op === '*') {
+    } else if (op === '*') {
         returnVal = multiply(numOne, numTwo)
-    } else if(op === '/') {
+    } else if (op === '/') {
         returnVal = divide(numOne, numTwo)
     }
     return returnVal;
